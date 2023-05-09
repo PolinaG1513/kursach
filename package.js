aa3fe741092927a7e5c4383a30bddb73e970f729
m2106701 <m2106701@edu.misis.ru>
2023-05-09 21:05:16 +0300

    first commit

---
+++ package.js
@@ -0,0 +1,28 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+canvas.style.width = "950px"
+canvas.style.heigth = "950px"
+ctx.fillStyle = "white";
+ctx.fillRect(15, 0, 800, 800);
+// function addtoStack() {
+//     ctx.beginPath();
+//     ctx.rect(100, 100, 300, 100);
+//     ctx.stroke();
+//     setTimeout("drawFrame()", 20);
+// }
+// ctx.font = "30px mono"
+// ctx.textAlign = "center";
+// ctx.strokeStyle = "red";
+// ctx.strokeText("Hello", 60, 60);
+
+// drawPoint(60, 60);
+
+// function drawPoint(x, y) {
+//     ctx.beginPath();
+//     ctx.arc(x, y, 2, 0, Math.PI * 2);
+//     ctx.fillStyle = "red";
+//     ctx.fill();
+// }
+
+
+
